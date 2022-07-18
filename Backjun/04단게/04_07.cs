4716affb01df1c196e0c2f635db10b67d8325f44
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-07-18 15:51:45 +0900

    2022/07/18 백준문제

--- Backjun/04단게/04_07.cs
+++ Backjun/04단게/04_07.cs
@@ -8,31 +8,31 @@ namespace Backjun._04단게
 {
     internal class _04_07
     {
-        static void Main(string[] args)
+        static void Ma1in(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());             // 입력받을 케이스의 갯수
 
             for (int i = 0; i< N; i++)
             {
-                string[] Starr = Console.ReadLine().Split();
-                double Student = int.Parse(Starr[0]);
-                double Avg = 0;
-                double Result = 0;
-                
-                for (int j = 1; j <= Student; j++)
-                {
-                    Avg += double.Parse(Starr[j]) / Student;
-                }
-                double Input = 1 / Student * 100;
+                string[] starr = Console.ReadLine().Split();   // 학생의 수  + 점수입력받기
+                int[] input = Array.ConvertAll(starr[1..],int.Parse);
+                double avg = 0;                                // 반 평균 점수
+                double result = 0;                             // 출력값
+                int num = 0;
 
-                for (int k = 1; k <= Student; k++)
+                avg = input.Average();
+
+                
+                for (int k = 0; k < input.Length; k++)
                 {
-                    if (Avg < int.Parse(Starr[k]))
+                    if (avg < input[k])           // Avg보다 높은 점수가 있는지 확인하기
                     {
-                        Result += Input;
+                        num++;                   // 높다면 인풋(1명이 반에서 가지는 퍼센트율)을 정답에 추가
                     }
                 }
-                Console.WriteLine("{0:.000}"+"%",Result);
+                result = (double)num / (double)input.Length * 100;
+                // 반에서 평균점수보다 높은 점수를 가진 사람의 비율 출력
+                Console.WriteLine("{0:0.000}"+"%",result); 
             }
         }
     }
---
+++ Backjun/06단계/06_01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjun._06단계
+{
+    internal class _06_01
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] ch = str.ToCharArray();
+            Console.WriteLine(ch[0] + 0);
+        }
+    }
+}
