2636aa9aed34e63f61162ad9bfd700883eadbc05
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-07-20 17:20:43 +0900

    2022/07/20 SQL 수업예제

---
+++ SQL/교대 3장 정렬과 연산.sql
@@ -0,0 +1,10 @@
+#날짜 : 2022/07/20
+#이름 : 김태언
+#내용 : 교재 3장 정렬과 연산
+
+CREATE TABLE `sample31`(
+  `name`    VARCHAR(10)
+  `age`     INT
+  `address`
+SELECT * FROM `sample31`;
+SELECT * FROM `sample31` ORDER BY `age`
