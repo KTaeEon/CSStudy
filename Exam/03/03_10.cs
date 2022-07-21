e41a93d56c509179f6b9f6d01dddc18c5f6c464e
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-07-21 17:21:04 +0900

    2022/07/21 연습문제

---
+++ Exam/03/03_10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam._03
+{
+    interface Buyer
+    {
+        public abstract void Buy();
+    }
+
+    interface Seller
+    {
+        public abstract void Sell();
+    }
+
+    class Coustomers : Buyer, Seller
+    {
+        public void Buy()
+        {
+            Console.WriteLine("구매하기");
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine("판매하기");
+        }
+    }
+
+    internal class _03_10
+    {
+        static void Mai1n(string[] args)
+        {
+            Buyer buyer = new Coustomers();
+            Seller seller = new Coustomers();
+
+            buyer.Buy();
+            seller.Sell();
+        }
+    }
+}
