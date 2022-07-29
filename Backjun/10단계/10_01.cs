0f29cb86ee5d851c9a36a6ad3ba77a4fb3e066fc
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-07-29 13:17:33 +0900

    2022/07/29 백준문제

---
+++ Backjun/10단계/10_01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjun._10단계
+{
+    internal class _10_01
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(arr[0]);
+            int m = int.Parse(arr[1]);
+            string[] arr2 = Console.ReadLine().Split();
+            List<int> list = new List<int>();
+
+            foreach (var item in arr2)
+            {
+                list.Add(int.Parse(item));
+            
+
+        }
+    }
+}
