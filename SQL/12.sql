d03e614829af127377daada720aecfb140ad1595
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-08-09 09:17:10 +0900

    2022/08/04 SQL

---
+++ SQL/12.sql
@@ -0,0 +1,8 @@
+INSERT INTO `user1` VALUES ( 'a101', '김유신', '010-1234-1001', 25);
+INSERT INTO `user1` VALUES ( 'a102', '김춘추', '010-1234-1002', 22);
+INSERT INTO `user1` VALUES ( 'a103', '장보고', '010-1234-1003', 35);
+INSERT INTO `user1` VALUES ( 'a104', '강감찬', '010-1234-1004', 45);
+INSERT INTO `user1` VALUES ( 'a105', '이순신', '010-1234-1005', 55);
+
+SELECT * FROM `user1`;
+
