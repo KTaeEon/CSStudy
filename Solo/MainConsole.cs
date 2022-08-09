07a41c7369a7c1e15c884e8698a9ea74dd26b8b1
rlaxodjsdlek <rlaxodjsdlek@gmail.com>
2022-08-09 09:15:39 +0900

    2022/08/04 개인프로젝트

---
+++ Solo/MainConsole.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solo
+{
+    internal class MainConsole
+    {
+        public static void Main(string[] args)
+        {
+            bool start = false;
+            bool inv = false;
+
+            int attack = 10;
+            int x = 0;
+            int y = 1;
+            int mct = 20;
+            int level = 1;
+            int potion = 0;
+            int stage = 1;
+
+            float exp = 0;
+            float max = 50;
+
+            int heart = 100;
+            int life = 3;
+
+            Console.WriteLine("숨은 몬스터 잡기 게임에 오신 유저분들을 환영합니다 !");
+            Thread.Sleep(1000);
+            Console.WriteLine("유저분들은 이제 맵에서 숨어있는 20마리 몬스터를 찾아야합니다 !");
+            Thread.Sleep(1000);
+            Console.WriteLine("총 체력과 목숨이 주어지며 몬스터를 잡을때마다 경험치를 획득합니다.");
+            Thread.Sleep(1000);
+            Console.WriteLine("몬스터 처치시 일정확률로 포션을 드랍합니다.");
+            Thread.Sleep(1000);
+            Console.WriteLine("열심히 생존하여 최대한 많이 잡아보세요 !");
+            Thread.Sleep(1000);
+            Console.WriteLine("                1) 모험 시작");
+            Console.WriteLine("                2) 게임 종료");
+            Console.Write("                입력 : ");
+            string f = Console.ReadLine();
+
+            switch (f)
+            {
+                case "1":
+                    start = true;
+                    break;
+                case "2":
+                    Console.WriteLine("게임을 종료합니다. 안녕히가세요!");
+                    start = false;
+                    break;
+            }
+            Random rand = new Random();
+            List<Monster> list = new List<Monster>();
+
+            for (int i = 0; i < mct; i++)
+            {
+                list.Add(new Monster(rand.Next(1, 70), rand.Next(1, 20)));
+            }
+
+            ConsoleKeyInfo key;
+
+            while (start)
+            {
+                Console.Clear();
+                if(mct == 0)
+                {
+                    Clear(ref x, ref y, ref heart, ref life, ref mct, ref list, ref stage);
+                }
+                if (x <= 2)
+                {
+                    x = 2;
+                }
+                else if (x >= 70)
+                {
+                    x = 70;
+                }
+                if (y < 2)
+                {
+                    y = 2;
+                }
+                else if (y >= 20)
+                {
+                    y = 20;
+                }
+
+                for (int i = 0; i <= 21; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("─────────────────────────────────────────────────────────────────────────────────────────");
+                        Console.WriteLine($"  현재 체력 : {heart}   남은 목숨 : {life} L   현재 공격력 : {attack}   남은 몬스터 {mct}마리  현재 레벨 Lv" + level);
+                        Console.WriteLine("─────────────────────────────────────────────────────────────────────────────────────────");
+                    }
+                    else if (i == 1)
+                    {
+                        Console.WriteLine(" ┌───────────────────────────────────────────────────────────────────────┐ 조작키 : W S A D ㅣ 인벤토리 : i");
+                    }
+                    else if (i == y)
+                    {
+                        Console.WriteLine(" {0, 1}{1," + x + "} {2, " + (71 - x) + "}", '│', 'P', '│');
+                    }
+                    else if (i == 21)
+                    {
+                        Console.WriteLine(" └───────────────────────────────────────────────────────────────────────┘");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" {0, 0} {1, 71}", '│', '│');
+                    }
+                }
+                if (inv)
+                {
+                    Console.WriteLine("현재남은 포션 : {0}개", potion);
+                }
+
+                for (int i = 0; i < 20; i++)
+                {
+                    bool ft = list[i].Mob(x, y);
+                    if (ft)
+                    {
+                        bool di = list[i].Fight(attack, ref heart, ref life, ref potion, ref exp, ref mct);
+                        list[i].die = di;
+                    }
+                }
+
+                if (exp >= max)
+                {
+                    level++;
+                    exp = exp % max;
+                    max += 10f;
+                    attack += 3;
+                    Console.WriteLine("레벨 업 !");
+                    Console.WriteLine("공격력이 3 증가합니다.");
+                }
+
+                key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.A:
+                        x--;
+                        break;
+                    case ConsoleKey.D:
+                        x++;
+                        break;
+                    case ConsoleKey.W:
+                        y--;
+                        break;
+                    case ConsoleKey.S:
+                        y++;
+                        break;
+                    case ConsoleKey.I:
+                        if (inv)
+                        { inv = false; }
+                        else
+                        { inv = true; }
+                        break;
+
+                }
+            }
+        }
+
+        public static void Clear(ref int x, ref int y, ref int heart, ref int life, ref int mct, ref List<Monster> list, ref int stage)
+        {
+            Console.WriteLine("몬스터를 다 잡으셨군요 ! 축하합니다!");
+            Console.WriteLine("단계를 더 높혀서 진행하시겠습니까 ?");
+            Console.WriteLine("1) 네. 단계를 높혀서 진행하겠습니다.");
+            Console.WriteLine("2) 아니요. 여기까지 할게요.");
+            Er:
+            string str = Console.ReadLine();
+            if (str == "1")
+            {
+                x = 0;
+                y = 1;
+                heart = 100;
+                life = 3;
+                mct = list.Count + 5;
+                stage++;
+                Console.WriteLine("난이도 2");
+            }
+            else if (str == "2")
+            {
+                Console.WriteLine("고생하셨습니다. 안녕히계세요 !");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("올바르지 못한 값 입니다. 다시 입력해주세요.");
+                goto Er;
+            }
+        }
+    }
+}
---
+++ Solo/Mob.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solo
+{
+
+    public class Monster
+    {
+        private int xr;
+        private int yr;
+        public bool die = false;
+
+        public Monster(int xr, int yr)
+        {
+            this.xr = xr;
+            this.yr = yr;
+        }
+
+
+        public bool Mob(int x, int y)
+        {
+            if (die == false)
+            {
+                if (x == xr && y == yr)
+                {
+                    Console.WriteLine("지나가다가 몬스터를 만났습니다 !");
+                    Console.WriteLine("싸우시겠습니까 ?");
+                ERROR1:
+                    Console.Write("1.예  2. 아니오 : ");
+                    try
+                    {
+                        string answer = Console.ReadLine();
+                        if (answer == "1" || answer == "2")
+                        {
+                            if (answer == "1")
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("도망쳤습니다.");
+                                Console.WriteLine("계속 진행하려면 아무키나 누르세요.");
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("정확한 값이 아닙니다 다시 입력해주세요");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        goto ERROR1;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool Fight(int attack, ref int heart, ref int life, ref int potion, ref float exp, ref int mct)
+        {
+            Random rand = new Random();
+            int ht = rand.Next(40, 50);
+            float ex = rand.Next(10, 15);
+            bool res = true;
+            ConsoleKeyInfo ky;
+            while (res)
+            {
+                int at = rand.Next(13, 18);
+                Console.WriteLine("몬스터 체력 : " + ht);
+                Console.WriteLine("나의 체력 : " + heart);
+                Console.WriteLine(" 공격 A , 방어 E , 포션 사용 H");
+                Task.Delay(10000);
+
+                while (Console.KeyAvailable)
+                {                    
+                    Console.ReadKey(true);           
+                }
+
+                ky = Console.ReadKey(true);
+
+                switch (ky.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.WriteLine("공격 !");
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"적에게 {attack}데미지 공격");
+                        Thread.Sleep(1000);
+                        ht -= attack;
+                        Console.WriteLine($"적이 {at}데미지 공격");
+                        Thread.Sleep(1000);
+                        heart -= at;
+                        break;
+                    case ConsoleKey.E:
+                        Console.WriteLine("방어 !");
+                        Thread.Sleep(1000);
+                        int luck = rand.Next(1, 5);
+                        if (luck == 2)
+                        {
+                            Console.WriteLine("행운 발동! 카운터 공격 !");
+                            Thread.Sleep(1000);
+                            Console.WriteLine("적에게 {0}데미지를 주었다", attack + 5);
+                            Thread.Sleep(1000);
+                            ht -= attack + 5;
+                        }
+                        else
+                        {
+                            Console.WriteLine("공격을 방어하여 {0}만큼 데미지를 입었다 !", at - 8);
+                            Thread.Sleep(1000);
+                            heart -= at - 8;
+                        }
+                        break;
+                    case ConsoleKey.H:
+                        if (potion > 0)
+                        {
+                            Console.WriteLine("포션 사용 !");
+                            heart += 50;
+                            if (heart > 100)
+                            {
+                                heart = 100;
+                            }
+                            Thread.Sleep(1000);
+
+                            potion--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("포션이 없습니다.");
+                            Thread.Sleep(1000);
+
+                        }
+                        
+                        break;
+                }
+                if (heart <= 0)
+                {
+                    Console.WriteLine("죽었습니다.");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("라이프가 있을경우 라이프를 소모하여 부활합니다.");
+                    Thread.Sleep(1000);
+                    if (life > 0)
+                    {
+                        heart = 100;
+                        life--;
+                        Console.WriteLine("남은 라이프 : " + life);
+                        Thread.Sleep(1000);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("라이프가 부족하여 부활할 수 없습니다 게임을 종료합니다");
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"당신은 {20 - mct}마리의 몬스터를 잡았습니다.");
+                        Thread.Sleep(1000);
+
+                        Environment.Exit(0);
+                    }
+                }
+                if (ht <= 0)
+                {
+                    Console.WriteLine("몬스터를 죽였습니다 !");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"{ex}만큼 경험치를 획득했습니다.");
+                    Thread.Sleep(1000);
+                    int px = rand.Next(1, 4);
+                    if (px == 3)
+                    {
+                        Console.WriteLine("포션을 얻었습니다 !");
+                        Thread.Sleep(1000);
+
+                        potion++;
+                    }
+                    exp += ex;
+                    mct--;
+                    Console.WriteLine($"남은 몬스터 {mct}마리");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("계속 진행하려면 아무키나 누르세요.");
+                    res = false;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
